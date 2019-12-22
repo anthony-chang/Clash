12f638d7101bb00e9913a8e346505cfebd448424
anthony-chang <54450499+anthony-chang@users.noreply.github.com>
2019-12-22 16:42:34 -0500

    responds to tilt correctly, bounces off screen walls

--- Testing/app/src/main/java/com/example/testing/GameView.java
+++ Testing/app/src/main/java/com/example/testing/GameView.java
@@ -2,59 +2,17 @@ package com.example.testing;
 
 import android.content.Context;
 import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
-public class GameView extends SurfaceView implements SurfaceHolder.Callback {
-    private MainThread thread;
-    public PlayerSprite p1;
+public class GameView extends SurfaceView {
 
     public GameView(Context context) {
         super(context);
-        getHolder().addCallback(this);
-        thread = new MainThread(getHolder(), this);
-        setFocusable(true);
     }
 
-    @Override
-    public void surfaceCreated(SurfaceHolder holder) {
-        p1 = new PlayerSprite();
-
-        thread.setRunning(true);
-        thread.start();
-    }
-
-    @Override
-    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-
-    }
-
-    @Override
-    public void surfaceDestroyed(SurfaceHolder holder) {
-        boolean retry = true;
-        while (retry) {
-            try {
-                thread.setRunning(false);
-                thread.join();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            retry = false;
-        }
-
-    }
-    public void update() {
-        p1.update();
-    }
     @Override
     public void draw(Canvas canvas) {
         super.draw(canvas);
-        if (canvas != null) {
-            canvas.drawColor(Color.LTGRAY);
-            p1.draw(canvas);
-
-        }
     }
+
 }
--- Testing/app/src/main/java/com/example/testing/PlayerSprite.java
+++ Testing/app/src/main/java/com/example/testing/PlayerSprite.java
@@ -1,38 +1,53 @@
 package com.example.testing;
 
-import android.app.Activity;
 import android.content.res.Resources;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 
 public class PlayerSprite {
-    private int x, y, width, height;
-    private int xVelocity = 10;
-    private int yVelocity = 5;
+    private int width, height;
+    private double x, y, vx, vy, ax, ay;
     private int screenWidth = Resources.getSystem().getDisplayMetrics().widthPixels;
-    private int screenHeight = Resources.getSystem().getDisplayMetrics().heightPixels;
+    private int screenHeight = Resources.getSystem().getDisplayMetrics().heightPixels-100;
 
     public PlayerSprite() {
-        x=100;
-        y = 100;
         width = 100;
         height = 100;
+        x=screenWidth/2;
+        y=screenHeight/2;
+        vx = vy = ax = ay = 0;
     }
-    public void update() {
-        x += xVelocity;
-        y += yVelocity;
+    public void update(Canvas canvas, double sensorX, double sensorY) {
+        ax = 0.5*sensorX;
+        ay = 0.5*sensorY;
+        vx+=ax;
+        vy+=ay;
+        x+=0.3*vx;
+        y+=0.3*vy;
         if (x > screenWidth - width || x < 0) {
-            xVelocity = xVelocity * -1;
+            vx *= -0.2;
+            //ax = 0;
+            if (x<0) x = 0;
+            if (x > screenWidth - width) x = screenWidth - width;
         }
         if (y > screenHeight - height || y < 0) {
-            yVelocity = yVelocity * -1;
+            vy *= -0.2;
+            //ay = 0;
+            if (y<0) y = 0;
+            if (y > screenHeight - height) y = screenHeight - height;
         }
+        draw(canvas);
 
     }
     public void draw(Canvas canvas) {
         Paint paint = new Paint();
-        paint.setColor(Color.rgb(0, 100, 100));
-        canvas.drawRect(x, y, x+width, y+height, paint);
+        paint.setColor(Color.rgb(0, 100, 255));
+        canvas.drawRect((int)x, (int)y, (int)x+width, (int)y+height, paint);
+
+        Paint textpaint = new Paint();
+        textpaint.setColor(Color.BLACK);
+        textpaint.setTextSize(50);
+        canvas.drawText(x+", "+y, 10, 40, textpaint);
     }
 }
