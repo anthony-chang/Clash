dd2b88de2cfa8be4cbd874237ee6c8368f3cece2
anthony-chang <54450499+anthony-chang@users.noreply.github.com>
2019-12-22 23:30:13 -0500

    create libgdx clash project files

---
+++ Clash-GDX/desktop/src/com/clash/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.clash.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.clash.Clash;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Clash(), config);
+	}
+}
