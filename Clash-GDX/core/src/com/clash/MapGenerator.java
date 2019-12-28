78943126272d39887a9a6a78c9acfeec56873dc0
anthony-chang <54450499+anthony-chang@users.noreply.github.com>
2019-12-28 17:41:20 -0500

    create MapGenerator class that reads from JSON file

---
+++ Clash-GDX/core/src/com/clash/MapGenerator.java
@@ -0,0 +1,32 @@
+package com.clash;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.JsonReader;
+import com.badlogic.gdx.utils.JsonValue;
+
+public class MapGenerator {
+    Array<Wall> walls = new Array<Wall>();
+    Array<Obstacle> obstacles = new Array<Obstacle>();
+
+    public MapGenerator(String fileName) {
+        JsonReader reader = new JsonReader();
+        JsonValue base = reader.parse(Gdx.files.internal(fileName));
+        for(JsonValue i : base.get("Walls")) {
+            walls.add(new Wall(
+                    i.getInt("x"),
+                    i.getInt("y"),
+                    i.getInt("width")/2,
+                    i.getInt("height")/2
+            ));
+        }
+        for(JsonValue i : base.get("Obstacles")) {
+            obstacles.add(new Obstacle(
+                    i.getInt("x"),
+                    i.getInt("y"),
+                    i.getInt("width")/2,
+                    i.getInt("height")/2
+            ));
+        }
+    }
+}
