e2ea0044795f6e65382f026989214318cabe065c
William Kang <51466389+w1ll37@users.noreply.github.com>
2020-01-03 00:03:19 -0500

    added MultiplayerDemo to test node.js server

---
+++ MultiplayerDemo/core/src/com/multiplayerdemo/MultiplayerDemo.java
@@ -0,0 +1,49 @@
+package com.multiplayerdemo;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+import io.socket.client.Socket;
+import io.socket.client.IO;
+
+
+public class MultiplayerDemo extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture img;
+	private Socket socket;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		img = new Texture("badlogic.jpg");
+		connectSocket();
+	}
+
+	@Override
+	public void render () {
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.begin();
+		batch.draw(img, 0, 0);
+		batch.end();
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+		img.dispose();
+	}
+
+	public void connectSocket(){
+		try{
+			socket = IO.socket("http://localhost:8080");
+			socket.connect();
+		}
+		catch(Exception e){
+			System.out.println(e);
+		}
+	}
+}
